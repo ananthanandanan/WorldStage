6a57379ad7e126c239286b430672e9a72c50fba1
ananthanandanan <ananthanandanan@gmail.com>
2021-02-04 04:45:52 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+### WorldStage 
+
+This is basically all the knowledge I have gained doing other django projects and recreating what 
+I have learned to from scratch to get the ground understanding how it all falls into place.
+
+### Tech used
+- Django
+- Graphql
+- Graphql-JWT
+- SQL
--- framework/framework/settings.py
+++ framework/framework/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hr^gresg9llj39i(27p39heag+8z%_^wcsgt+l-9dq93hlk8b('
+SECRET_KEY = 'hr^gresg9llj39i(27p39heag+8z%_^wcsgt+l-9dq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
